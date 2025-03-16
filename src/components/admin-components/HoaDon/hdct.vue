667f6d637a90c6e3a5e35446f061ffccbb1d011e
hawin22 <146153289+hawin22@users.noreply.github.com>
2025-03-16 22:57:34 +0700

    thu

--- src/components/admin-components/HoaDon/hdct.vue
+++ src/components/admin-components/HoaDon/hdct.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="container-fluid">
-        <span class="text-center pb-2">Thông tin chi tiết đơn hàng </span>
+        <span class="text-center pb-2">Thông tin chi tiết đơn hàng: </span>
+        <hr>
 
     </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-const route = useRoute();
-const id = route.params.id;
-import { useGbStore } from '@/stores/gbStore';
-const store = useGbStore();
-const hoaDon = ref(store.getAllHoaDonArr.find(hoaDon => hoaDon.id_hoa_don === id));
+// import { useRoute } from 'vue-router';
+// const route = useRoute();
+// const id = route.params.id;
+// import { useGbStore } from '@/stores/gbStore';
+// const store = useGbStore();
+// const hoaDon = ref(store.getAllHoaDonArr.find(hoaDon => hoaDon.id_hoa_don === id));
 // {{  hoaDon ? hoaDon.ma_hoa_don : 'Không tìm thấy hóa đơn'  }}
 
 </script> 
